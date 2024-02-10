5f18da58aefe5290048d73ec41f104d86e3babea
Mirucat-max <82355970+Mirucat-max@users.noreply.github.com>
2024-02-10 17:26:28 +0300

    Update CORS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "https://example.com",
+    origin: "https://pixelviatka.onrender.com",
     methods: ["GET", "POST"]
   }
 });
